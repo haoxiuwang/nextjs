fdd29513b40a054a27e1f9179f9c3fc79df414b0
haoxiu wang <whxcctv@gmail.com>
2021-11-27 00:32:23 +0800

    update

--- pages/fetch.js
+++ pages/fetch.js
@@ -1,6 +1,6 @@
 import React, {useEffect,useState} from 'react'
 
-export default function () {
+export default function Myfetch () {
   var [data,setData] = useState(null)
   useEffect(()=>{
     async function fetchdata() {
@@ -9,6 +9,7 @@ export default function () {
         var files = await res.json()
         setData(files.data)
     }
+    if(data)return;
     fetchdata()
   },[data])
   if(!data)return 'nothing'
