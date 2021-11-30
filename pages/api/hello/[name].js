50513399b8e5e187108973271b9a54ecc2ec6b3e
haoxiu wang <>
2021-11-30 10:10:41 +0800

    update

--- pages/api/hello/[name].js
+++ pages/api/hello/[name].js
@@ -4,8 +4,8 @@ const path = require('path');
 const ss = require('serve-static')(path.join(process.cwd(),htmls));
 export default function handler(req, res) {
   var {id} = req.query;
-  req.url+='/'+id
-  ss(req,res,function () {
-    res.end('hello!');
-  })
+  res.end(req.url)
+  // ss(req,res,function () {
+  //   res.end('hello!');
+  // })
 }
