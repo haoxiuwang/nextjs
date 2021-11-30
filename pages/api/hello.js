98ed2b4a91eb7c81f18d9590b0d8bb02890b30f8
haoxiu wang <>
2021-11-30 09:44:39 +0800

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const fs = require('fs');
 const path = require('path');
+const ss = require('serve-static')(path.join(process.cwd(),htmls));
 export default function handler(req, res) {
-
-  var data = fs.readdirSync(path.join(process.cwd(),'pages/docs'));
-
-    console.log(data,'-------');
-  res.status(200).json({data})
-
+  ss(req,res,function () {
+    res.end('hello!');
+  })
 }
