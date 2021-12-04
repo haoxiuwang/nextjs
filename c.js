acf71801ee45e563070805f2242bacb9c9e6b89c
H.X. Wang <whxcctv@gmail.com>
2021-12-04 18:51:01 +0800

    update

--- c.js
+++
@@ -1,6 +0,0 @@
-const {createReadStream,createWriteStream} = require('fs');
-var [_,_,f] = process.argv
-const path = require('path');
-var rs = createReadStream(path.join(process.cwd(),'example.md'))
-var ws = createWriteStream(path.join(process.cwd(),f))
-rs.pipe(ws)
