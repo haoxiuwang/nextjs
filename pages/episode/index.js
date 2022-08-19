8ff285ce12703a70b473d6bdbe1d7d2861db9a63
haoxiu wang <whxcctv@gmail.com>
2022-08-19 21:08:36 +0800

    update

--- pages/episode/index.js
+++ pages/episode/index.js
@@ -1,36 +1,47 @@
 import { useState, useEffect } from "react"
 import { Image } from "next/image"
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 export default function Episode() {
   const router = useRouter()
   const { serie,season,episode,count,path } = router.query
   const [index,setIndex] = useState(-1)
 
-
-  const [parts,setParts] = useState(null)
+  console.log({path,index});
+  const [{parts,error0},setParts] = useState({parts:null,error0:null})
 
   useEffect(()=>{
     if (!episode)return
     var url = `${path}/source.json`
-
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=>setParts(data))
+    .then((data)=>setParts({parts:data,error0:null}))
+    .catch((error)=>setParts({parts:null,error0:error}))
   },[episode])
-  const [blob,setBlob] = useState(null)
+
+
+  const [{blob,error1},setBlob] = useState({blob:null,error1:null})
   useEffect(()=>{
-    if(index>-1)
+    if(index<0)return
+    setBlob({blob:null,error1:null})
     fetch(`${path}/${index}.mp3`)
     .then((res)=>res.blob())
-    .then((data)=>setBlob(data))
+    .then((data)=>setBlob({blob:data,error1:null}))
+    .catch((error)=>setBlob({blob:null,error1:error}))
   },[index])
-  const [time,setTime] = useState(null)
+
+
+  const [{time,error2},setTime] = useState({time:null,error2:null})
   useEffect(()=>{
-    if(index>-1)
+    if(index<0)return
+    setTime({time:null,error2:null})
     fetch(`${path}/${index}.json`)
     .then((res)=>res.json())
-    .then((data)=>setTime(data))
+    .then((data)=>setTime({time:data,error2:null}))
+    .catch((error)=>setTime({time:null,error2:error}))
   },[index])
+
+
   function list(count) {
     var i = 0
     var arr = []
@@ -48,10 +59,12 @@ export default function Episode() {
     setPlayer(new Audio())
   },[episode])
 
+  if (!parts)return(<div>loading source.json...</div>)
+  if (error0)return(<div>loading source.json error...</div>)
   if(index==-1){
-
   return (
-    <div className="container episode" style={{minHeight:"100vh"}}>
+    <div>
+    <div className="episode" style={{minHeight:"calc(100vh - 80px)"}}>
       <div className="episode">
       <div style={{textAlign:"center",marginBottom:"25px"}}>This episode include {count} parts, please start with one of them:</div>
       <div>
@@ -59,8 +72,12 @@ export default function Episode() {
       </div>
       </div>
     </div>
+    <div style={{backgroundColor:"#eceff1",height:"80px"}} className="flex_row_center"><Link href="/"><a>Back to Home Page</a></Link></div>
+    </div>
   )}
-
+  if (!time)return(<div>loading time data...</div>)
+  if (!blob)return(<div>loading audio data...</div>)
+  if (error1||error2)return(<div>time and audio data error...</div>)
   var part = parts[index]
 
   return(
@@ -77,36 +94,40 @@ function Card({texts,blob,time,player,back}) {
   const [repeat,setRepeat] = useState(false)
 
   useEffect(()=>{
-    if (!blob||!player)return
+    if (index<0){
+      if(!player.paused)
+      player.pause()
+      player.ontimeupdate = null
+      back(-1)
+      return
+    }
     player.ontimeupdate = ()=>{
+      if(index==time.length-1)return
       if(player.currentTime>(time[index+1].timeSeconds-0.8)){
         console.log({index,time:player.currentTime,time1:time[index+1].timeSeconds-0.8});
         player.pause()
       }
     }
     player.src = window.URL.createObjectURL(blob)
-
-    return function () {
-      if(!blob||!player)return
-      player.ontimeupdate = null
-    }
   },[index])
   useEffect(()=>{
-    if(!player||index<0)return
-    player.pause()
+    if (index<0){
+      if(!player.paused)
+      player.pause()
+      player.ontimeupdate = null
+      back(-1)
+      return
+    }
+    if(!player.paused)
+      player.pause()
     player.currentTime = time[index].timeSeconds
     player.play()
-
   },[index,repeat])
 
   return(
     <div>
-    <div onClick={(e)=>{
-      setIndex(Math.floor(texts.length*e.screenX/window.innerWidth))
-    }} className="progress">
-      <div style={{marginRight:value}}></div>
-    </div>
-      <div  onClick={(e)=>{
+      <Progress setIndex={setIndex} length={time.length}/>
+      <div onClick={(e)=>{
         console.log({e});
         var n = window.innerHeight*0.2
         var m = window.innerHeight*0.3
@@ -118,10 +139,29 @@ function Card({texts,blob,time,player,back}) {
         setRepeat(!repeat)
 
       }} className="episode" style={{minHeight:"calc(100vh - 110px)",padding:"25px"}}>
-        {texts[index].en}
+        {index>-1&&texts[index].en}
       </div>
-      <div style={{backgroundColor:"#eceff1",height:"80px"}} className="flex_row_center" onClick={()=>back(-1)}>Back to Parts</div>
+      <div style={{backgroundColor:"#eceff1",height:"80px"}} className="flex_row_center" onClick={()=>{
+        setIndex(-1)
+      }}>Back to Parts</div>
 
+    </div>
+  )
+}
+
+function Progress({setIndex,length}) {
+  const [ratio,setRatio] = useState(0)
+  return(
+    <div onClick={(e)=>{
+      var ratio = e.screenX/window.innerWidth
+      var value = Math.floor(length*ratio)
+      if(value>length-1)value=length-1
+      setRatio(ratio)
+      setIndex(value)
+    }} style={{height:"30px"}}>
+      <div style={{backgroundColor:"pink"}}>
+        <div style={{width:ratio*100+"%",backgroundColor:"red",height:"3px"}}></div>
+      </div>
     </div>
   )
 }
