82a345935fe39d6bdeae7941de19d6dfbd259906
haoxiu wang <whxcctv@gmail.com>
2022-08-22 10:06:52 +0800

    update

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+export default function Text() {
+  var arr = ["Fantasic_Mr._fox","Martian","Mom","VOA_Making_Of_Nation","White_Collar"]
+  const [index,setIndex] = useState(-1)
+  console.log(index);
+  if(index<0)
+  return (
+    <div className="test_flex">
+      {arr.map((item,i)=>(<div style={{margin:"15px"}} key={i} onClick={()=>setIndex(i)}>{item}</div>))}
+    </div>
+  )
+  if(index>-1)
+  return(<div onClick={()=>setIndex(-1)} className="test_flex"><img width="100" src={`/series/${arr[index]}/cover.jpg`}/></div>)
+}
