e9da01b183bbbcff6a1350f94a2a711e61dbd063
haoxiu wang <whxcctv@gmail.com>
2022-09-12 20:31:52 +0800

    update

--- components/card.js
+++ components/card.js
@@ -41,7 +41,7 @@ export default function Card({height,texts,blob,time,player,back,path}) {
   return(
   <div className="select-none" style={{height:height+"px"}}>
     <div onClick={()=>setAuto(!auto)} className="fixed right-[25px] top-[25px]">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-record-circle ${auto&&"text-sky-400"}`} viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className={`bi bi-record-circle ${auto&&"text-sky-400"}`} viewBox="0 0 16 16">
 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
 </svg>
