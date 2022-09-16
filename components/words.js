089dbebe7d6b88a2fc693e420297b7941c6170a1
haoxiu wang <whxcctv@gmail.com>
2022-09-16 18:23:00 +0800

    update

--- components/words.js
+++ components/words.js
@@ -20,28 +20,28 @@ export default function Words({texts,index,str,setFav,path}) {
   useEffect(()=>setNotes([]),[word.en])
   if(mem)
   return(
-    <div className="fixed h-full inset-0 m-8 overflow-y-auto">
-    <div>
-      <svg onClick={()=>setMem(false)} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-arrow-left text-sky-400 font-bold" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-      </svg>
-    </div>
-    <ul className="block bg-sky-100 my-6 p-3 rounded outline-3 outline-slate-300 outline-offset-2">
-    {
-      local.map((item,i)=>{
-        return(
-        <li key={i} className="block">
-          <span>{item.en}</span><span>{item.zh}</span>
-        </li>
-        )
-      })
-    }
-    </ul>
+    <div onTouchStart={()=>alert("start")} className="fixed h-full inset-0 my-8 overflow-y-auto">
+      <div>
+        <svg onClick={()=>setMem(false)} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-arrow-left text-sky-400 font-bold" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+        </svg>
+      </div>
+      <ul className="block bg-sky-100 m-6 p-3 rounded outline-3 outline-slate-300 outline-offset-2">
+      {
+        local.map((item,i)=>{
+          return(
+          <li key={i} className="block">
+            <span>{item.en}</span><span>{item.zh}</span>
+          </li>
+          )
+        })
+      }
+      </ul>
     </div>
   )
   return (
       <div className="m-3 mt-16">
-        <div className="fixed left-3 top-3 right-3 flex place-content-around">
+        <div className="fixed left-3 top-3 right-3 flex place-content-between">
           <svg onClick={()=>setFav(false)} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-arrow-left text-sky-400 font-bold" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
           </svg>
