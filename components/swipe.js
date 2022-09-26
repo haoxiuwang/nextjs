091c320b7a6c31946996bbe8cee4a8f67206aa0e
haoxiu wang <whxcctv@gmail.com>
2022-09-26 12:13:46 +0800

    update

--- components/swipe.js
+++ components/swipe.js
@@ -57,11 +57,11 @@ export default function ({height,auto,setAuto,fz,setFz,index,setIndex,time,repea
       <div style={{transform:`rotate(-${rnd}deg)`}} className={`relative p-3 py-10 m-auto w-[20rem] md:w-[25rem] text-center bg-sky-200 border-solid rounded border-sky-400 border-1`}>
         {lang?en:zh}
       </div>
-      {_local.length>0&&(<div className={`relative p-3 py-10 m-3 w-[20rem] md:w-[25rem] text-center bg-sky-100 border-solid rounded border-sky-400 border-1`}>
+      {_local.length>0&&(<div style={{transform:`rotate(-${Math.floor(Math.random()*5)}deg)`}} className={`relative p-3 py-10 m-3 w-[20rem] md:w-[25rem] text-center bg-sky-100 border-solid rounded border-sky-400 border-1`}>
         {
           _local.map((item,i)=>{
           return(
-            <div style={{transform:`translate(${Math.floor(Math.random()*5)})`}} key={i}>
+            <div key={i}>
               <span className="text-semibold">{item.en}</span>
               <span>{item.zh}</span>
             </div>
