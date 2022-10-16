22a51e52a573a09e40a5a9ba5a9b9e6e31a4f4fd
haoxiu wang <whxcctv@gmail.com>
2022-10-16 23:30:20 +0800

    update

---
+++ pages/pingshu.js
@@ -0,0 +1,8 @@
+export default function Pingshu() {
+
+  return (
+    <div>
+      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" src="//music.163.com/outchain/player?type=4&id=350331992&auto=1&height=430"></iframe>
+    </div>
+  )
+}
