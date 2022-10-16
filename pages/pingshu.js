d42634056842c8e5f48174e2fc6351c29754a8db
haoxiu wang <whxcctv@gmail.com>
2022-10-16 23:35:58 +0800

    update

--- pages/pingshu.js
+++ pages/pingshu.js
@@ -2,7 +2,7 @@ export default function Pingshu() {
 
   return (
     <div>
-      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" src="//music.163.com/outchain/player?type=4&id=350331992&auto=1&height=430"></iframe>
+      <iframe className="w-screen h-screen" frameborder="no" border="0" marginwidth="0" marginheight="0" src="//music.163.com/outchain/player?type=4&id=350331992&auto=1&height=430"></iframe>
     </div>
   )
 }
