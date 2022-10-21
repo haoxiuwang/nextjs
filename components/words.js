03037bbf5244d0434e4b2f0ab0b8aa1c4c3ad178
haoxiu wang <whxcctv@gmail.com>
2022-10-21 15:17:21 +0800

    update

--- components/words.js
+++ components/words.js
@@ -52,7 +52,7 @@ export default function Words({height,texts,index,str,setFav,_key}) {
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-book text-sky-800" viewBox="0 0 16 16">
                 <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
               </svg>
-              <span className="ml-3" onClick={()=>window.open(`https://dictionary.cambridge.org/us/dictionary/english-chinese-simplified/?q=${word.en.trim().indexOf(" ")>-1?word.en.trim().split(" ").join("+"):word.en.trim()}`,'_blank')}>C</span>
+              <span className="ml-3" onClick={()=>window.open(`https://idioms.thefreedictionary.com?${word.en.trim().indexOf(" ")>-1?word.en.trim().split(" ").join("+"):word.en.trim()}`,'_blank')}>C</span>
               <span className="ml-3" onClick={()=>window.open(`https://www.bing.com/dict/search?q=${word.en.trim()}&mkt=zh-cn`,'_blank')}>B</span>
             </div>
             <input className="appearance-none min-w-0 text-base text-sky-600 placeholder:text-slate-600 focus:outline-none sm:text-sm sm:leading-6" value={word.en} onChange={(e)=>setWord({en:e.target.value,zh:""})}/>
