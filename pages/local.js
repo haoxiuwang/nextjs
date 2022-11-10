e400dfcff30fd9034d4b3894114b813c2d58b627
haoxiu wang <whxcctv@gmail.com>
2022-11-10 13:37:14 +0800

    update

--- pages/local.js
+++ pages/local.js
@@ -1,10 +1,18 @@
 
 import {useState,useEffect,useRef} from "react"
 export default function Local() {
-
+  const [local,setLocal] = useState(null);
   const download = useRef(null)
   return(
     <div className="fixed inset-0 flex space-y-5 flex-col place-content-center place-items-center">
+      <div>
+        {
+          Object.keys(local).map((key,i)=>(
+          <div>{key}</div>
+          <div>JSON.stringify({local[key]})</div>
+        ))
+      }
+      </div>
       <div className="text-center">把网页数据保存到本地文件“local.md”,或把"local.md"文件上传到网页。</div>
       <div className="space-x-3 flex place-content-center place-items-center">
         <div className="rounded bg-slate-100 py-2 px-5" onClick={()=>{
@@ -27,9 +35,9 @@ export default function Local() {
             }
           })
           .then((buf)=>{
-            var local = new TextDecoder("utf-8").decode(buf)
-            local = JSON.parse(local)
-            console.log(local);
+            var _local = new TextDecoder("utf-8").decode(buf)
+            _local = JSON.parse(local)
+            setLocal(_local)
             // var keys = Object.keys(local)
             // console.log(keys);
             // keys.forEach((key, i) => {
